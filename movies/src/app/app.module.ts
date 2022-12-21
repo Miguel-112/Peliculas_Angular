d05e3bf3a6356cc9f5c4ccaa5620c7ebf95c2e6d
Miguel-112 <71301712+Miguel-112@users.noreply.github.com>
2022-12-21 14:54:35 -0500

    Se crearon los componentes necesarios para la aplicación
    Se crearon los componentes necesarios para la aplicación .

--- movies/src/app/app.module.ts
+++ movies/src/app/app.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './modules/login/login.component';
+
+import { MoviesComponent } from './modules/movies/movies.component';
+import { MovieDetailComponent } from './modules/movies/movie-detail/movie-detail.component';
+import { BaseComponent } from './shared/base/base.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MoviesComponent,
+    MovieDetailComponent,
+    BaseComponent
   ],
   imports: [
     BrowserModule
